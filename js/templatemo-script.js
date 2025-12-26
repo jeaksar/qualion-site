144b6239837ca85a2372c173efd3f6065725b243
Jeaksar Rasho <jeaksr@gmail.com>
2025-12-26 14:29:59 +0100

    Adjust language toggle styles for better layout and visibility; update background transition duration for smoother effect

--- js/templatemo-script.js
+++ js/templatemo-script.js
@@ -2,7 +2,7 @@ const initBg = (autoplay = true) => {
     const bgImgsNames = ['diagoona-bg-1.jpg', 'diagoona-bg-2.jpg', 'diagoona-bg-3.jpg'];
     const bgImgs = bgImgsNames.map(img => "images/" + img);
 
-    $.backstretch(bgImgs, {duration: 4000, fade: 1500});
+    $.backstretch(bgImgs, {duration: 5000, fade: 1500});
 
     if (!autoplay) {
         $.backstretch('pause');
